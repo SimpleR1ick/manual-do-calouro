dc065f401ab9fdd863984898e1e1abd741c22e21
Maria Eduarda de S. Barros <maria123duarda@gmail.com>
2022-07-12 15:47:23 -0300

    Layout estático de Horários

---
+++ Prog Wb2/manual_do_calouro/horarios.php
@@ -0,0 +1,169 @@
+<section>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-auto">
+                <h4>Selecione o curso e o módulo:</h4>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-auto">
+                
+                <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                    <label class="btn btn-success active">
+                      <input type="radio" name="curso" id="info" autocomplete="off" checked> INFO
+                    </label>
+            
+                    <label class="btn btn-success">
+                        <input type="radio" name="curso" id="med" autocomplete="off"> MEC
+                    </label>
+            
+                    <label class="btn btn-success">
+                        <input type="radio" name="curso" id="iot" autocomplete="off"> IOT
+                    </label>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="row m-3 justify-content-center">
+            <div class="col-auto">
+
+                <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                    <label class="btn btn-success active">
+                      <input type="radio" name="modulo" id="mod1" autocomplete="off" checked> 1
+                    </label>
+                    
+                    <label class="btn btn-success">
+                        <input type="radio" name="modulo" id="mod2" autocomplete="off"> 2
+                    </label>
+                    
+                    <label class="btn btn-success">
+                        <input type="radio" name="modulo" id="mod3" autocomplete="off"> 3
+                    </label>
+                    
+                    <label class="btn btn-success">
+                        <input type="radio" name="modulo" id="mod4" autocomplete="off"> 4
+                    </label>
+                    
+                    <label class="btn btn-success">
+                        <input type="radio" name="modulo" id="mod5" autocomplete="off"> 5
+                    </label>
+                    
+                    <label class="btn btn-success">
+                        <input type="radio" name="modulo" id="mod6" autocomplete="off"> 6
+                    </label>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-sm-auto">
+                
+                <div class="table-responsive">
+                    <table class="table table-responsive-sm table-bordered table-striped text-center">
+                        <thead class="thead-light">
+                            <tr>
+                                <th></th>
+                                <th scope="col">SEG</th>
+                                <th scope="col">TER</th>
+                                <th scope="col">QUA</th>
+                                <th scope="col">QUI</th>
+                                <th scope="col">SEX</th>
+                                <th scope="col">SÁB</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="align-middle">7:30<br>8:20</td>
+                                <td>
+                                    <span class="d-flex flex-column">
+                                        <small >Sala N°</small>
+                                        <strong>DISCIPLINA</strong>
+                                        <small>Professor</small>
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="d-flex flex-column">
+                                        <small >Sala N°</small>
+                                        <strong>DISCIPLINA</strong>
+                                        <small>Professor</small>
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="d-flex flex-column">
+                                        <small >Sala N°</small>
+                                        <strong>DISCIPLINA</strong>
+                                        <small>Professor</small>
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="d-flex flex-column">
+                                        <small >Sala N°</small>
+                                        <strong>DISCIPLINA</strong>
+                                        <small>Professor</small>
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="d-flex flex-column">
+                                        <small >Sala N°</small>
+                                        <strong>DISCIPLINA</strong>
+                                        <small>Professor</small>
+                                    </span>
+                                </td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td class="align-middle">8:20<br>9:10</td>
+                                <td>
+                                    <span class="d-flex flex-column">
+                                        <small >Sala N°</small>
+                                        <strong>DISCIPLINA</strong>
+                                        <small>Professor</small>
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="d-flex flex-column">
+                                        <small >Sala N°</small>
+                                        <strong>DISCIPLINA</strong>
+                                        <small>Professor</small>
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="d-flex flex-column">
+                                        <small >Sala N°</small>
+                                        <strong>DISCIPLINA</strong>
+                                        <small>Professor</small>
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="d-flex flex-column">
+                                        <small >Sala N°</small>
+                                        <strong>DISCIPLINA</strong>
+                                        <small>Professor</small>
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="d-flex flex-column">
+                                        <small >Sala N°</small>
+                                        <strong>DISCIPLINA</strong>
+                                        <small>Professor</small>
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="d-flex flex-column">
+                                        <small >Sala N°</small>
+                                        <strong>DISCIPLINA</strong>
+                                        <small>Professor</small>
+                                    </span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
