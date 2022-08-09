fc445c9f4ff7cb673eea3c79c99ca197563d848c
Henrique Dalmagro <84425302+SimpleR1ick@users.noreply.github.com>
2022-08-09 15:37:04 -0300

    Fix ElephantSQL

--- Prog Wb2/manual_do_calouro/php/includes/connect.php
+++ Prog Wb2/manual_do_calouro/php/includes/connect.php
@@ -1,4 +1,5 @@
 <?php
+/*
 // Dados para conexão com banco de dados local
 $host = "localhost";
 $port = "5432";
@@ -7,19 +8,18 @@ $user = "postgres";
 $pass = "252804277353"; // Senha variavel 
 
 $connect = pg_connect("host=$host port=$port dbname=$db user=$user password=$pass");
+*/
 
-// Definindo a conexão como uma constante global
-define('_CONEXAO_', $connect)
-
-/*
 // Dados para conexão com o banco de elephatnSQL
 $host = "kesavan.db.elephantsql.com";
 $user = "jqvnqgvm";
 $db = "jqvnqgvm";
 $pass = "IKXmlWoguZUV-Rav8eB14MghUUXaZAk8";
 
-
 // Conexão com banco de dados
 $connect = pg_connect("host=$host dbname=$db user=$user password=$pass");
-*/
+
+// Definindo a conexão como uma constante global
+define('_CONEXAO_', $connect)
+
 ?>
