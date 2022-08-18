04fd897036b78f6c3c400f09fb07f370b998d0de
Maria Eduarda de S. Barros <maria123duarda@gmail.com>
2022-08-18 15:31:02 -0300

    Update horários Bootstrap 5

--- Prog Wb2/manual_do_calouro/horarios.php
+++ Prog Wb2/manual_do_calouro/horarios.php
@@ -7,68 +7,52 @@
 <!-- Conteudo da pagina -->
 <section>
     <div class="container">
-        <div class="d-flex justify-content-center">
-            <div class="btn-group" role="group">
-                <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
-                <label class="btn btn-outline-primary" for="btnradio1">INFO</label>
 
-                <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-                <label class="btn btn-outline-primary" for="btnradio2">MEC</label>
-
-                <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
-                <label class="btn btn-outline-primary" for="btnradio3">IOT</label>
-            </div>
-
-            <div class="btn-group" data-toggle="buttons">
-
+        <form class="d-flex flex-column align-items-center" action="" method="get">
+            <h5>Curso:</h5>
+            <!-- GRUPO DE BOTÕES CURSO -->
+            <div class="btn-group mb-3" role="group">
+                <!-- INFORMÁTICA -->
                 <input type="radio" class="btn-check" name="curso" id="info" autocomplete="off">
-                <label class="btn btn-secondary" for="info">INFO</label>
+                <label class="btn btn-success" for="info">INFO</label>
 
-                <label class="infoLabel btn btn-success active">
-                    <input type="radio" name="curso" class="horario" id="info" autocomplete="off" /> INFO
-                </label>
+                <!-- INTERNET DAS COISAS -->
+                <input type="radio" class="btn-check" name="curso" id="iot" autocomplete="off">
+                <label class="btn btn-success" for="iot">IOT</label>
 
-                <label class="mecLabel btn btn-success">
-                    <input type="radio" name="curso" class="horario" id="med" autocomplete="off" /> MEC
-                </label>
+                <!-- MECATRÔNICA -->
+                <input type="radio" class="btn-check" name="curso" id="meca" autocomplete="off">
+                <label class="btn btn-success" for="meca">MEC</label>
 
-                <label class="iotLabel btn btn-success ">
-                    <input type="radio" name="curso" class="horario" id="iot" autocomplete="off" /> IOT
-                </label>
             </div>
-        </div>
 
-        <div class="row m-3 justify-content-center">
-            <div class="col-auto">
-                <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                    <label class="label1 btn btn-success active">
-                        <input type="radio" name="modulo" class="horario" id="mod1" autocomplete="off" /> 1
-                    </label>
+            <h5>Módulo:</h5>
+            <!-- GRUPO DE BOTÕES MÓDULO -->
+            <div class="btn-group mb-3" role="group">
+                <input type="radio" class="btn-check" name="modulo" id="mod1" autocomplete="off">
+                <label class="btn btn-success" for="mod1">1°</label>
 
-                    <label class="label2 btn btn-success">
-                        <input type="radio" name="modulo" class="horario" id="mod2" autocomplete="off" /> 2
-                    </label>
+                <input type="radio" class="btn-check" name="modulo" id="mod2" autocomplete="off">
+                <label class="btn btn-success" for="mod2">2°</label>
 
-                    <label class="label3 btn btn-success">
-                        <input type="radio" name="modulo" class="horario" id="mod3" autocomplete="off" /> 3
-                    </label>
+                <input type="radio" class="btn-check" name="modulo" id="mod3" autocomplete="off">
+                <label class="btn btn-success" for="mod3">3°</label>
 
-                    <label class="label4 btn btn-success">
-                        <input type="radio" name="modulo" class="horario" id="mod4" autocomplete="off" /> 4
-                    </label>
+                <input type="radio" class="btn-check" name="modulo" id="mod4" autocomplete="off">
+                <label class="btn btn-success" for="mod4">4°</label>
 
-                    <label class="label5 btn btn-success">
-                        <input type="radio" name="modulo" class="horario" id="mod5" autocomplete="off" /> 5
-                    </label>
+                <input type="radio" class="btn-check" name="modulo" id="mod5" autocomplete="off">
+                <label class="btn btn-success" for="mod5">5°</label>
 
-                    <label class="label6 btn btn-success">
-                        <input type="radio" name="modulo" class="horario" id="mod6" autocomplete="off" /> 6
-                    </label>
-                </div>
+                <input type="radio" class="btn-check" name="modulo" id="mod6" autocomplete="off">
+                <label class="btn btn-success" for="mod6">6°</label>
             </div>
-        </div>
 
-        <div class="row justify-content-center">
+            <!-- BOTÃO DE PESQUISA -->
+            <button class="btn btn-info" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+        </form>
+
+        <div class="row justify-content-center mt-3 mb-5">
             <div class="col-sm-auto">
                 <div class="table-responsive">
                     <table class="table table-responsive-sm table-bordered table-striped text-center">
