030c6f73f3a64999fc8bfc9a5e22deb2c6d77287
Henrique Dalmagro <84425302+SimpleR1ick@users.noreply.github.com>
2022-08-19 12:53:16 -0300

    Salvar imagem

--- mdc/perfil.php
+++ mdc/perfil.php
@@ -16,6 +16,7 @@
 
                         <img id="foto-editar-perfil" class="foto-editar-perfil p-3" src="user.png" alt="user-pic">
 
+                        <input type="hidden" name="MAX_FILE_SIZE" value="33554432">
                         <input class="mt-2 form-control form-control-sm" id="nova-foto-perfil" name="foto" type="file">
                     </picture>
                 </div>
--- mdc/php/functions/upload.php
+++ mdc/php/functions/upload.php
@@ -1,9 +1,9 @@
 <?php
-// Caminho da pasta com imagens dos usuarios
-$uploadDir = "img/perfil/";
+// Inicia a sessão
+session_start();
 
-// Definindo a variavel com path como constante
-define('UPLOADS', $uploadDir);
+// Conexão com banco de dados
+require_once '../includes/connect.php';
 
 uploadFoto();
 
@@ -14,15 +14,26 @@ uploadFoto();
  * @author Henrique Dalmagro
  */
 function uploadFoto() {
-    $fotoNome = pg_escape_string($_FILES['foto']['name']);
-    $fotoTpmNome = $_FILES['foto']['tmp_name'];
+    if (isset($_SESSION['id_usuario'])) {
+        $id = $_SESSION['id_usuario'];
 
-    $fotoTamanho = $_FILES['foto']['size'];
-    
-    $fotoNovoNome = rand().$fotoNome;
+        $fotoNome = pg_escape_string(CONNECT, $_FILES['foto']['name']);
 
-    echo $fotoTpmNome;
-    echo $fotoNovoNome;
-    
+        $fotoTpmNome = $_FILES['foto']['tmp_name'];
+        $fotoTamanho = $_FILES['foto']['size'];
+
+        $fotoNewName = $id . $fotoNome;
+
+        //echo filetype($fotoTpmNome);
+        //echo $fotoNome . "<br>";
+        //echo $fotoTpmNome . "<br>";
+        //echo $fotoTamanho . "<br>";
+
+        if ($fotoTamanho < $_POST['MAX_FILE_SIZE']) {
+            $sql = "UPDATE usuario SET img_perfil='' WHERE id_usuario = '$id'";
+            //$query = pg_query(CONNECT, $sql)
+            move_uploaded_file($fotoTpmNome, "../../img/perfil/" . $fotoNewName);
+        }
+    }    
 }
 ?>
