8a5dfcbc67dc526fd1e07ab049e3b072ed8e2225
Maria Eduarda de S. Barros <maria123duarda@gmail.com>
2022-08-26 12:59:37 -0300

    Update Contatos

--- mdc/contatos.php
+++ mdc/contatos.php
@@ -1,56 +1,48 @@
 <!-- Header-->
-<?php include_once 'php/includes/header.php';?>
+<?php include_once 'php/includes/header.php'; ?>
 
-<!-- Conteúdo da pagina -->
 <section>
-    <div class="mb-4">
-        <div class="container">
-            <div class="d-flex justify-content-center">
-                <div class="card shadow-sm px-sm-4 py-2 mb-5 bg-light">
+    <div class="container d-flex flex-column align-items-center">
+        <div class="card mb-3 card-contato">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="img/perfil/user.png" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body border border-danger">
+                            <h5 class="card-title fw-bold">Prof. Fulano</h5>
+                            
+                            <div class="row">
+                                <div class="col-12 py-1">
+                                    <i class="fa-solid fa-phone"></i>
+                                    <span>(27) 99999-9999</span>
+                                </div>
 
-                <div class="card-body">
-
-                    <div class="h1 text-center">Fale conosco</div>
-                        <form>
-
-                            <div class="mb-3">
-                                <label class="form-label" for="email">E-mail:</label>
-                                <input type="email" class="form-control" id="email" aria-describedby="ajudaEmail" placeholder="Seu email" required>
+                                <div class="col-12 py-1">
+                                    <span>
+                                        <i class="fa-solid fa-envelope"></i>
+                                        <span>profbello@ifes.edu.br</span>
+                                    </span>
+                                </div>
                             </div>
 
-                            <div class="mb-3">
-                                <label class="form-label" for="telefone">Telefone:</label>
-                                <input type="tel" class="form-control" id="telefone" placeholder="Seu telefone" minlength="8" maxlength="11" required>
-                            </div>
+                            <div class="row">
+                                <div class="col-5">
+                                    
+                                </div>
 
-                            <div class="mb-3">
-                                <label class="form-label" for="assunto">Assunto:</label>
-                                <select class="form-select" name="assunto" id="assunto" required>
-                                    <option value="">Selecione um assunto</option>
-                                    <option value="calendario">Calendário</option>
-                                    <option value="horarios">Horários</option>
-                                    <option value="mapa">Mapa</option>
-                                    <option value="rod">ROD</option>
-                                    <option value="critica">Crítica/Reclamação</option>
-                                    <option value="outro">Outro</option>
-                                </select>
-                            </div>
+                                <div class="col-5">
+                                    
+                                </div>
 
-                            <div class="mb-2">
-                                <label class="form-label" for="texto">Escreva sua mensagem:</label><br>
-                                <textarea class="w-100" id="texto" name="texto" required rows="6"></textarea>
-                            </div>
+                                <div class="col-2">
 
-                            <div class="d-flex justify-content-center">
-                                <button type="submit" class="btn btn-primary">Enviar</button>
+                                </div>
                             </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
-
-<!-- Footer -->
-<?php include_once 'php/includes/footer.php';?>
