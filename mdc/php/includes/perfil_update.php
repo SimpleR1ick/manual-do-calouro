2f1e653d1171f67b47b46349a5ce3e6417f87115
Henrique Dalmagro <84425302+SimpleR1ick@users.noreply.github.com>
2022-09-01 16:26:40 -0300

    Update perfil

--- mdc/php/includes/perfil_update.php
+++ mdc/php/includes/perfil_update.php
@@ -2,12 +2,12 @@
 // Iniciar a sessão
 session_start();
 
+// Conexão com banco de dados
+require_once 'connect.php';
+
 // Import de bibliotecas de funções
 include_once '../functions/sanitizar.php';
 include_once '../functions/upload.php';
-
-// Conexão com banco de dados
-require_once 'connect.php';
 
 if (isset($_POST['btnIncrement'])) {
     if (sanitizaPost($_POST)) {
@@ -16,26 +16,53 @@ if (isset($_POST['btnIncrement'])) {
     
     }
     if ($_POST['acesso'] == 1) {
-        // Atribui o conteudo obtido dos campos do formulario a variáveis]
-        $curso = pg_escape_string(CONNECT, $_POST['curso']);
-        $modulo = pg_escape_string(CONNECT, $_POST['modulo']);
-        $nome = pg_escape_string(CONNECT, $_POST['nome']);
-        $email = pg_escape_string(CONNECT, $_POST['email']);
-
-        inserirCursoModulo($curso, $modulo);
-
-        atualizaDadosUsuario($nome, $email);
+        perfilProfessor();
 
     } else if ($_POST['acesso'] == 2) {
-        $regras = pg_escape_string(CONNECT, $_POST['regras']);
-        $nome = pg_escape_string(CONNECT, $_POST['nome']);
-        $email = pg_escape_string(CONNECT, $_POST['email']);
+        perfilAluno();
     }
-    $_SESSION['sucess'] = 'Perfil atualizado com sucesso!';
-    header('Location: ../../perfil.php');
+}
+ 
+/**
+ * 
+ * 
+ * 
+ * @author Rafael Barros - Henrique Dalmagro
+ */
+function perfilProfessor(): void {
+    $regras = pg_escape_string(CONNECT, $_POST['regras']);
 
-    $_SESSION['mensag'] = 'Erro ao atualizar perfil!';
-    header('Location: ../../perfil.php');
+    // Query para fazer o update das informações do professor
+    $sql = "INSERT INTO professor (fk_servidor_fk_usuario_id_usuario, regras) VALUES
+            ({$_SESSION['id_usuario']}, $regras)";
+}
+
+/**
+ * Função para inserir o curso e o modulo do aluno
+ * 
+ * @param string $curso código único do curso
+ * @param string $modulo código único do módulo
+ * 
+ * @author Rafael Barros - Henrique Dalmagro
+ */
+function perfilAluno(): void {
+    // Atribui o conteudo obtido dos campos do formulario a variáveis]
+    $modulo = pg_escape_string(CONNECT, $_POST['modulo']);
+    $curso = pg_escape_string(CONNECT, $_POST['curso']);
+
+    // Fazer verificação se o aluno ja esta cadastrado
+
+    // Query para fazer o update das informações do aluno
+    $sql = "INSERT INTO aluno (fk_usuario_id_usuario, fk_turma_id_turma) VALUES
+            ({$_SESSION['id_usuario']},
+            (SELECT id_turma
+            FROM turma
+            WHERE num_modulo = $modulo
+            AND fk_curso_id_curso = $curso))";
+
+    if (pg_query(CONNECT, $sql)) {
+        
+    }
 }
     
 /**
@@ -46,41 +73,20 @@ if (isset($_POST['btnIncrement'])) {
  * 
  * @author Henrique Dalmagro
  */
-function atualizaDadosUsuario($nome, $email): void {
+function atualizaDadosUsuario(): void {
+    $nome = pg_escape_string(CONNECT, $_POST['nome']);
+    $email = pg_escape_string(CONNECT, $_POST['email']);
+
     // Query para fazer o update das informações do usuário
     $sql = "UPDATE usuario SET nom_usuario = '$nome', email = '$email' 
             WHERE id_usuario = {$_SESSION['id_usuario']}";
 
-}
-/**
- * Função para inserir o curso e o modulo do aluno
- * 
- * @param string $curso código único do curso
- * @param string $modulo código único do módulo
- * 
- * @author Rafael Barros
- */
-function inserirCursoModulo($curso, $modulo): void {
-    // Query para fazer o update das informações do aluno
-    $sql = "INSERT INTO aluno (fk_usuario_id_usuario, fk_turma_id_turma) VALUES
-            ({$_SESSION['id_usuario']},
-            (SELECT id_turma
-            FROM turma
-            WHERE num_modulo = $modulo
-            AND fk_curso_id_curso = $curso))";
-
-    // Verfica se o update funcionou e guarda uma mensagem de acordo
     if (pg_query(CONNECT, $sql)) {
-        
-
+        $_SESSION['sucess'] = 'Perfil atualizado com sucesso!';
+        header('Location: ../../index.php');
     } else {
-        
+        $_SESSION['mensag'] = 'Erro ao atualizar perfil!';
+        header('Location: ../../perfil.php');
     }
-}
-
-function inserirRegras(): void {
-    // Query para fazer o update das informações do professor
-    $sql = "INSERT INTO professor (fk_servidor_fk_usuario_id_usuario, regras) VALUES
-            ()"
 }
 ?>
