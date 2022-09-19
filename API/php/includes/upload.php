150c42af89fd2a7a5fbc6fe9136bc3ca4699e479
Henrique Dalmagro <henriquedalmagro04@gmail.com>
2022-09-19 09:13:32 -0300

    Update upload.php

--- API/php/includes/upload.php
+++ API/php/includes/upload.php
@@ -1,37 +1,52 @@
 <?php
+session_start();
+
+define('DIR', 'assets/uploads/');
 
 // Nome da foto, nome temporario no servidor e tamanho da foto
-$nome_foto = pg_escape_string(CONNECT, $_FILES['foto']['name']);
-$nome_temp = $_FILES['foto']['tmp_name'];
+$foto_nome = $_FILES['foto']['name'];
 $foto_size = $_FILES['foto']['size'];
-
-$sql = "SELECT img_perfil FROM usuario WHERE id_usuario = '{$_SESSION['id_usuario']}'";
-$db_path = pg_query(CONNECT, $sql);
-
-if ($db_path == null) {
-    // Transforma em um array os dados da foto ('dirname', 'basename', 'extension', 'filename')
-    $path = pathinfo($nome_foto);
-
-    // Renomeando a foto com o id do usuario e mantendo a extensão do arquivo
-    $nome_final = time().'.'.$path['extension'];
-
-} else {
-    // Utiliza o mesmo nome do banco para atualizar a foto
-    $nome_final = $db_path;
-}
-
+$path_temp = $_FILES['foto']['tmp_name'];
 
 // Verifica se o tamanho da foto esta no limite permitido
-if ($foto_size < $_POST['MAX_FILE_SIZE']) {
-    $sql = "UPDATE usuario SET img_perfil ='$novo_nome' WHERE id_usuario = ID";
+if ($foto_size > $_POST['MAX_FILE_SIZE']) {
+    $_SESSION['mensag'] = 'Tamanho limite da foto foi excedido';
+
+} else {
+    $sql = "UPDATE usuario SET img_perfil ='$nome_final' 
+            WHERE id_usuario = '{$_SESSION['id_usuario']}'";
 
     if (pg_query(CONNECT, $sql)) {
-        if (move_uploaded_file($nome_temp, 'assets/uploads/'.$novo_nome)) {
-            $_SESSION['sucess'] = 'Foto atualizada com sucesso!';
-        }
+        move_uploaded_file($nome_temp, DIR.$nome_final);
+        $_SESSION['sucess'] = 'Foto atualizada com sucesso!';
+
     } else {
         $_SESSION['mensag'] = 'Erro ao atualizar foto!';
     }
-} 
-$_SESSION['mensag'] = 'Tamanho limite da foto foi excedido';
+}    
+
+/**
+ * 
+ * @param string $nome_foto
+ * 
+ */
+function nomeFoto($nome_foto): void {
+    $sql = "SELECT img_perfil FROM usuario WHERE id_usuario = '{$_SESSION['id_usuario']}'";
+    $query = pg_query(CONNECT, $sql);
+
+    if ($query != null) {
+        $result = pg_fetch_all($query);
+
+        // Utiliza o mesmo nome do banco para atualizar a foto
+        $nome_final = $result['img_perfil'];
+    
+    } else {
+        // Transforma em um array os dados da foto ('dirname', 'basename', 'extension', 'filename')
+        $path = pathinfo($nome_foto);
+
+        // Renomeando a foto com o id do usuario e mantendo a extensão do arquivo
+        $nome_final = time().'.'.$path['extension'];
+    }
+    return $nome_final;
+}
 ?>
