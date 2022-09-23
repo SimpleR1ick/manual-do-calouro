316149b92504f8a7a5b6436d0d1f157fcd0e5f6d
Maria Eduarda de S. Barros <maria123duarda@gmail.com>
2022-09-23 16:47:13 -0300

    Update imagem do popover login

--- API/web/testepopover.php
+++ API/web/testepopover.php
@@ -1,21 +1,15 @@
-<!-- Header-->
-<?php require_once '../php/layout/header.php'; ?>
-
 <section class="container">
 
-<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    <img id="img-perfil" class="img-contato" src="../assets/images/user.png" alt="" srcset="">
-  </button>
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
-  </ul>
-</div>
+    <div class="dropdown">
+        <button class="btn border-none dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <img id="img-perfil" src="../assets/images/user.png" alt="" srcset="">
+        </button>
 
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+        </ul>
+    </div>
 
 </section>
-
-<!-- Footer -->
-<?php include_once '../php/layout/footer.php'; ?>
